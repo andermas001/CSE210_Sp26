150c0ebb62872b2f0ce4063632da719c8d077c04
andermas001 <and23076@byui.edu>
2026-04-29 14:30:08 -0600

    Prep 4 finnished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("we have a program to calculate a list of numbers");
+        Console.WriteLine("please enter a number to add, enter 0 to end the program");
+        int number;
+        List<int> numbers = new List<int>();
+        int total = 0;
+        int largest = 0;
+        do
+        {
+           Console.Write("please enter a number: ");
+           number = Convert.ToInt32(Console.ReadLine());
+           if (number != 0)
+           {
+           numbers.Add(number); 
+           }
+           if (number > largest)
+            {
+                largest = number;
+            }
+
+        }
+        while (number != 0);
+        int entries = numbers.Count;
+        Console.WriteLine($"the list has {entries} entries");
+       
+        foreach (int i in numbers){
+        total += i;    
+        Console.WriteLine(i);
+        }
+        Console.WriteLine($"The Largest number is {largest}");
+        Console.WriteLine($"The list adds up to {total}");
+        int average = total / entries;
+        Console.WriteLine($"The average is {average}");
+
+
     }
 }
