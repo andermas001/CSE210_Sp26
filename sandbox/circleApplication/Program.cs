3ffdf448e3da2b0704a88d34292aecafbae44cc4
andermas001 <and23076@byui.edu>
2026-05-04 14:44:09 -0600

    Circle class

---
+++ sandbox/circleApplication/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+
+    Circle myCircle = new Circle();
+    myCircle.SetRadius(10);
+    Console.WriteLine(myCircle.GetArea());
+
+    }
+}
---
+++ sandbox/circleApplication/circle.cs
@@ -0,0 +1,18 @@
+using System.Formats.Asn1;
+
+class Circle
+{
+    double _radius;
+
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+
+    }
+
+    public double GetArea()
+    {
+       return Math.PI * _radius * _radius;
+
+    }
+}
