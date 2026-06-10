5c5f18b22f8797f3916f07299478fde9da902501
andermas001 <and23076@byui.edu>
2026-06-10 14:26:49 -0600

    initial start

---
+++ sandbox/spinner/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+class Program
+{
+    
+     static void Main(string[] args)
+    {
+
+        double count = 15;
+        // string animationString = "\\|/";
+        DateTime now = DateTime.Now;
+        DateTime endtime = DateTime.AddSeconds(count);
+
+
+        while (count > 0)
+        {
+            
+        }
+        Console.WriteLine("" + now.ToString(""));
+        // int index = 0;
+       
+        int sleepTime = 1000;
+
+        while(DateTime.Now < endtime)
+        {
+            Console.Write($"{count--,2}");
+            Thread.Sleep(sleepTime);
+            Console.Write("\\b");
+        }
+
+    }
+}
